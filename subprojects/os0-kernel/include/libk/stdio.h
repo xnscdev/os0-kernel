bf93aee9995fcde70779b4a08384227f98ddce69
xnscdev <xnscdev@gmail.com>
2021-01-30 15:25:39 -0800

    Added libk itoa() and empty printk()

---
+++ subprojects/os0-kernel/include/libk/stdio.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * stdio.h -- This file is part of OS/0.                                 *
+ * Copyright (C) 2020 XNSC                                               *
+ *                                                                       *
+ * OS/0 is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * OS/0 is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with OS/0. If not, see <https://www.gnu.org/licenses/>.         *
+ *************************************************************************/
+
+#ifndef _LIBK_STDIO_H
+#define _LIBK_STDIO_H
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+void printk (const char *__restrict fmt, ...);
+
+__END_DECLS
+
+#endif
---
+++ subprojects/os0-kernel/libk/itoa.c
@@ -0,0 +1,54 @@
+/*************************************************************************
+ * itoa.c -- This file is part of OS/0.                                  *
+ * Copyright (C) 2020 XNSC                                               *
+ *                                                                       *
+ * OS/0 is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * OS/0 is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with OS/0. If not, see <https://www.gnu.org/licenses/>.         *
+ *************************************************************************/
+
+#include <libk/stdlib.h>
+
+char *
+itoa (int value, char *result, int base)
+{
+  char *ptr = result;
+  char *rev = result;
+  int temp;
+  char c;
+
+  if (base < 2 || base > 36)
+    {
+      *result = '\0';
+      return result;
+    }
+
+  do
+    {
+      temp = value;
+      value /= base;
+      *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210"
+	"123456789abcdefghijklmnopqrstuvwxyz"[35 + (temp - value * base)];
+    }
+  while (value != 0);
+
+  if (temp < 0)
+    *ptr++ = '-';
+  *ptr-- = '\0';
+  while (rev < ptr)
+    {
+      c = *ptr;
+      *ptr-- = *rev;
+      *rev++ = c;
+    }
+  return result;
+}
