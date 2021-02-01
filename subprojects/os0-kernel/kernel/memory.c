79b51a52d60b6b310a2b08176795911ebb1ba22a
xnscdev <xnscdev@gmail.com>
2021-02-01 11:27:37 -0800

    Added memory bitmap searching functions

--- subprojects/os0-kernel/kernel/memory.c
+++ subprojects/os0-kernel/kernel/memory.c
@@ -21,6 +21,43 @@
 
 extern void *_kernel_end;
 
+static u8 o5[1];
+static u8 o4[1];
+static u8 o3[1];
+static u8 o2[2];
+static u8 o1[4];
+static u8 o0[8];
+static u8 *block_list[] = {o0, o1, o2, o3, o4, o5};
+
+static int
+mem_get_bit (u32 order, int index, int bit)
+{
+  return block_list[order][index] & (1 << (bit - 1));
+}
+
+static void
+mem_set_bit (u32 order, int index, int bit)
+{
+  block_list[order][index] &= (1 << (bit - 1));
+}
+
+static int
+mem_search_free (u32 order, int *index, int *bit)
+{
+  size_t i;
+  for (i = 0; i < sizeof block_list[order]; i++)
+    {
+      u8 num = block_list[order][i];
+      int nbit = fls (num);
+      if (nbit == 0)
+	continue;
+      *index = i;
+      *bit = nbit;
+      return 0;
+    }
+  return -1;
+}
+
 void
 mem_init (u32 mem)
 {
@@ -32,5 +69,14 @@ mem_init (u32 mem)
 void *
 kmalloc (size_t size, u32 flags)
 {
+  u8 order = 0;
+  int index;
+  int bit;
+
+  while (1 << (order + 2) < size)
+    order++;
+
+  if (mem_search_free (order, &index, &bit) != 0)
+    return NULL; /* No blocks that can fit size bytes are available */
   return NULL;
 }
