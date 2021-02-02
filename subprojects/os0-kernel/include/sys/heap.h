62148617089935c11b2c202c2e0cc96ee5eb0d9b
xnscdev <xnscdev@gmail.com>
2021-02-01 18:30:18 -0800

    Moved heap structures to sys/heap.h

--- subprojects/os0-kernel/include/sys/heap.h
+++ subprojects/os0-kernel/include/sys/heap.h
@@ -1,5 +1,5 @@
 /*************************************************************************
- * memory.h -- This file is part of OS/0.                                *
+ * heap.h -- This file is part of OS/0.                                  *
  * Copyright (C) 2020 XNSC                                               *
  *                                                                       *
  * OS/0 is free software: you can redistribute it and/or modify          *
@@ -16,26 +16,35 @@
  * along with OS/0. If not, see <https://www.gnu.org/licenses/>.         *
  *************************************************************************/
 
-#ifndef _SYS_MEMORY_H
-#define _SYS_MEMORY_H
+#ifndef _SYS_HEAP_H
+#define _SYS_HEAP_H
 
-#include <libk/array.h>
-#include <sys/cdefs.h>
-#include <stddef.h>
+#include <sys/memory.h>
 
-#define MEM_MAGIC 0xefbeadde
-#define MEM_CIGAM 0xdeadbeef
+#define MEM_PAGEALIGN (1 << 0)
 
-#define MEM_MAX_BLOCK_ORDER 16 /* 256 MiB blocks */
-#define MEM_PAGESIZE 0x1000
+typedef struct
+{
+  u32 mh_magic;
+  u32 mh_size;
+  u8 mh_alloc;
+  u8 mh_reserved[7];
+} __attribute__ ((packed)) MemHeader;
 
-__BEGIN_DECLS
+typedef struct
+{
+  u32 mf_cigam;
+  u32 mf_header;
+} __attribute__ ((packed)) MemFooter;
 
-void mem_init (u32 mem);
-
-void *mem_alloc (size_t size, u32 flags);
-void mem_free (void *ptr, size_t size);
-
-__END_DECLS
+typedef struct
+{
+  SortedArray mh_index;
+  u32 mh_saddr;
+  u32 mh_eaddr;
+  u32 mh_maddr;
+  u16 mh_supvsr;
+  u16 mh_rdonly;
+} __attribute__ ((packed)) MemHeap;
 
 #endif
