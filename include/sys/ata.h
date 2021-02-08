73e2060fcfc69e45926b6cc365bbcf08a40f3fc9
xnscdev <xnscdev@gmail.com>
2021-02-07 21:17:10 -0800

    Added ATA definitions

--- include/sys/ata.h
+++ include/sys/ata.h
@@ -28,4 +28,72 @@
 #define ATA_SR_IDX  0x02
 #define ATA_SR_ERR  0x01
 
+#define ATA_ER_BBK  0x80
+#define ATA_ER_UNC  0x40
+#define ATA_ER_MC   0x20
+#define ATA_ER_IDNF 0x10
+#define ATA_ER_MCR  0x08
+#define ATA_ER_ABRT 0x04
+#define ATA_ER_T0NF 0x02
+#define ATA_ER_AMNF 0x01
+
+#define ATA_CMD_READ_PIO        0x20
+#define ATA_CMD_READ_PIO_EXT    0x24
+#define ATA_CMD_READ_DMA        0xc8
+#define ATA_CMD_READ_DMA_EXT    0x25
+#define ATA_CMD_WRITE_PIO       0x30
+#define ATA_CMD_WRITE_PIO_EXT   0x34
+#define ATA_CMD_WRITE_DMA       0xca
+#define ATA_CMD_WRITE_DMA_EXT   0x35
+#define ATA_CMD_CACHE_FLUSH     0xe7
+#define ATA_CMD_CACHE_FLUSH_EXT 0xea
+#define ATA_CMD_PACKET          0xa0
+#define ATA_CMD_IDENTIFY_PACKET 0xa1
+#define ATA_CMD_IDENTIFY        0xec
+
+#define ATAPI_CMD_READ  0xa8
+#define ATAPI_CMD_EJECT 0x1b
+
+#define ATA_IDENT_DEVICETYPE   0
+#define ATA_IDENT_CYLINDERS    2
+#define ATA_IDENT_HEADS        6
+#define ATA_IDENT_SECTORS      12
+#define ATA_IDENT_SERIAL       20
+#define ATA_IDENT_MODEL        54
+#define ATA_IDENT_CAPABILITIES 98
+#define ATA_IDENT_FIELDVALID   106
+#define ATA_IDENT_MAX_LBA      120
+#define ATA_IDENT_COMMANDSETS  164
+#define ATA_IDENT_MAX_LBA_EXT  200
+
+#define IDE_ATA   0
+#define IDE_ATAPI 1
+
+#define ATA_MASTER 0
+#define ATA_SLAVE  1
+
+#define ATA_REG_DATA     0x00
+#define ATA_REG_ERROR    0x01
+#define ATA_REG_FEATURES 0x01
+#define ATA_REG_SECCNT0  0x02
+#define ATA_REG_LBA0     0x03
+#define ATA_REG_LBA1     0x04
+#define ATA_REG_LBA2     0x05
+#define ATA_REG_HDDEVSEL 0x06
+#define ATA_REG_COMMAND  0x07
+#define ATA_REG_STATUS   0x07
+#define ATA_REG_SECCNT1  0x08
+#define ATA_REG_LBA3     0x09
+#define ATA_REG_LBA4     0x0a
+#define ATA_REG_LBA5     0x0b
+#define ATA_REG_CONTROL  0x0c
+#define ATA_REG_ALTSTAT  0x0c
+#define ATA_REG_DEVADDR  0x0d
+
+#define ATA_PRIMARY   0
+#define ATA_SECONDARY 1
+
+#define ATA_READ  0
+#define ATA_WRITE 1
+
 #endif
