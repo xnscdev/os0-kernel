abafefcbb57b6499121171d0a68ea48592d9ac7c
xnscdev <xnscdev@gmail.com>
2021-06-02 10:59:30 -0700

    Removed GDB pretty-printers

--- config/kernel-gdb.py
+++
@@ -1,7 +0,0 @@
-# GDB pretty-printer auto-load script
-
-import gdb.printing
-import printers
-
-gdb.printing.register_pretty_printer(gdb.current_objfile(),
-                                     printers.build_pretty_printer())
